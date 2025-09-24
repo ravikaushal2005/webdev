f23fe53ab38f1ce0eb674fd18e61380666deec66
RaviKaushal2005 <ravikaushal7284@gmail.com>
2025-09-24 17:06:21 +0530

    maths

---
+++ maths.js
@@ -0,0 +1,16 @@
+console.log("maths")
+console.log(Math)
+console.log(Math.PI)  // 3.141592653589793
+console.log(Math.abs(-4))  // 4   absolute value  // only negative to positive
+
+console.log(Math.round(4.6))  // 5   round to nearest integer
+console.log(Math.round(4.3))  // 4
+console.log(Math.ceil(4.3))  // 5   round up to next integer
+console.log(Math.floor(4.7))  // 4   round down to previous integer
+
+console.log(Math.min(0,150,30,20,-8,-200))  // -200   // find minimum number
+console.log(Math.max(0,150,30,20,-8,-200))  // 150   // find maximum number
+console.log(Math.pow(2,3))  // 8   // 2 to the power of 3
+console.log(2**3)  // 8   // 2 to the power of 3
+
+console.log(Math.random())  // random number between 0 to 1
